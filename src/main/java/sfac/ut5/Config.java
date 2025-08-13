b53c5472e4587b23935ec70e8e9399bea318da35
zer0M1nd <735790901@qq.com>
2025-08-13 15:19:35 +0800

    Update Config.java

--- src/main/java/sfac/ut5/Config.java
+++ src/main/java/sfac/ut5/Config.java
@@ -13,6 +13,11 @@ public class Config {
             .comment("The initial time(ticks) when player first enters the game")
             .defineInRange("initialTime", 3 * TimeSystem.HOUR, 0L, Long.MAX_VALUE);
 
+    public static final ModConfigSpec.BooleanValue ENABLE_RITUAL = BUILDER
+            .comment("If set to true, then players need to perform a ritual to start the contents of this mod.",
+                    "Otherwise, all contents, including time count-down, are started by default.")
+            .define("enableRitual", false);
+
     // Modified by zer0M1nd: ensure all fields initialized before build
     static ModConfigSpec build(){
         return BUILDER.build();
