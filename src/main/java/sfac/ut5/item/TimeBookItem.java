d57d814ef8d80a540e4208d7d57e9e4870915548
Mukai757 <757117633@qq.com>
2025-08-14 21:05:36 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/underneathtimev/Config.java
    #	src/main/java/underneathtimev/TimeSystem.java
    #	src/main/java/underneathtimev/UnderNeathTimeV.java
    #	src/main/java/underneathtimev/event/EventHandler.java
    #	src/main/java/underneathtimev/event/HelloWorldEvents.java
    #	src/main/java/underneathtimev/event/UTVEvents.java
    #	src/main/java/underneathtimev/event/UpdatePlayerTimeEvents.java
    #	src/main/java/underneathtimev/event/helloworld.java
    #	src/main/resources/assets/ut5/lang/en_us.json
    #	src/main/resources/assets/ut5/lang/zh_cn.json

---
+++ src/main/java/sfac/ut5/item/TimeBookItem.java
@@ -0,0 +1,28 @@
+package sfac.ut5.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import sfac.ut5.gui.UTVGUITypes;
+
+public class TimeBookItem extends Item {
+
+    public TimeBookItem(Properties properties) {
+        super(properties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        if (!level.isClientSide && interactionHand == InteractionHand.MAIN_HAND) {
+            // 在服务端打开GUI
+            player.openMenu((MenuProvider) UTVGUITypes.TIME_ANVIL_MENU.get());
+            //记得改成书的Gui，TODO
+            return InteractionResultHolder.success(player.getItemInHand(interactionHand));
+        }
+        return InteractionResultHolder.pass(player.getItemInHand(interactionHand));
+    }
+}
+
--- src/main/java/sfac/ut5/item/UTVItems.java
+++ src/main/java/sfac/ut5/item/UTVItems.java
@@ -76,6 +76,9 @@ public class UTVItems {
 
     public static final DeferredItem<TimePetalsItem> TimePetalsItem =
             register("backtrack_compass", TimePetalsItem::new, new Item.Properties().stacksTo(1), UnderneathTimeV.MAIN_TAB);
+
+    public static final DeferredItem<TimeBookItem> TimeBookItem =
+            register("backtrack_compass", TimeBookItem::new, new Item.Properties().stacksTo(1), UnderneathTimeV.MAIN_TAB);
     public static void init() {
     }
 
